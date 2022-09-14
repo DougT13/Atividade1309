1c96ee792e529f87cb3438a5db8577651fe902db
DougT13 <dougfs2004@gmail.com>
2022-09-13 21:55:47 -0300

    FUNFOU

--- app/src/main/java/br/com/etecia/atividade1309/CoffeFragment.java
+++ app/src/main/java/br/com/etecia/atividade1309/CoffeFragment.java
@@ -1,20 +1,49 @@
 package br.com.etecia.atividade1309;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoffeFragment extends Fragment {
+
+    RecyclerView listaCoffe;
+
+    List<Cafe> cafe;
+   // Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_coffe, container, false);
+
+        listaCoffe = view.findViewById(R.id.idRecyclerViewCoffe);
+
+        cafe = new ArrayList<>();
+
+        cafe.add(new Cafe("Café Expresso", "Um ótimo café", R.drawable.cafeexpresso));
+        cafe.add(new Cafe("Café Americano", "Um café bem americanizado", R.drawable.cafeamericano));
+        cafe.add(new Cafe("Café Breve", "Um café rápido", R.drawable.cafebreve));
+        cafe.add(new Cafe("Café com Leite", "Um café tradicional (mas com leite)", R.drawable.cafecomleite));
+        cafe.add(new Cafe("Café Irlandês", "Um café gringo", R.drawable.cafeirlandes));
+
+        AdapterCoffe adapterCoffe = new AdapterCoffe(getContext(), cafe);
+
+        listaCoffe.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        listaCoffe.hasFixedSize();
+
+        listaCoffe.setAdapter(adapterCoffe);
 
         return view;
     }
